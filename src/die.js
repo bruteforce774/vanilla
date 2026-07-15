ba9539c0d6c68e9403fba379de71d37b247652e6
Daniel Andreas Wang <codingforpeace@protonmail.com>
2026-07-15 15:38:25 +0200

    add setDie

--- src/die.js
+++ src/die.js
@@ -1,3 +1,8 @@
 export function setupDie(element) {
   let die = 0
+  const setDie = (dieValue) => {
+    die = dieValue
+    element.innerHTML = die ? `Rolled: ${die}` : `Die not rolled.`
+  }
 }
+
