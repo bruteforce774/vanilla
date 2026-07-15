710d93facf352af99775ab7e8332d20a7bbe0e43
Daniel Andreas Wang <codingforpeace@protonmail.com>
2026-07-15 16:50:31 +0200

    complete die roll button with RNG

--- src/die.js
+++ src/die.js
@@ -4,5 +4,7 @@ export function setupDie(element) {
     die = dieValue
     element.innerHTML = die ? `Rolled: ${die}` : `Die not rolled.`
   }
+  element.addEventListener('click', () => setDie(Math.floor(Math.random() * 6) + 1))
+  setDie(0)
 }
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { setupCounter } from './counter.js'
+import { setupDie } from './die.js'
 
 document.querySelector('#app').innerHTML = `
 <button id="counter" type="button" class="counter"></button>
+<button id="die" type="button" class="die"></button>
 `
 
 setupCounter(document.querySelector('#counter'))
+setupDie(document.querySelector('#die'))
